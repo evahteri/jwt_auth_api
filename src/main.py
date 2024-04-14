8da189ad215ba3c63eb728b882faad455472f7e7
Eero Vahteri <eero.vahteri@helsinki.fi>
2024-04-14 16:13:48 +0300

    format and documentation

--- src/main.py
+++ src/main.py
@@ -9,20 +9,17 @@ from services.custom_expiration_validator import ExpirationValidator
 
 app = FastAPI()
 
-
 class JWTValidity(BaseModel):
     valid: bool
 
-
 @app.get("/auth", response_model=JWTValidity)
 async def index(Authorization: Annotated[str | None, Header()]) -> JWTValidity:
-    jwt_validity = True
-    token_headers = JWTDecoder(token=Authorization).get_token_headers()
-    jwt_validity = HeaderValidator(headers=token_headers).validate_headers()
-    token_payload = await SignatureValidator(token=Authorization, x5u=token_headers["x5u"]).validate_signature()
-    jwt_validity = ExpirationValidator(
-        token_payload=token_payload).validate_expiration()
-    return JWTValidity(valid=jwt_validity)
+    token_headers = JWTDecoder(token=Authorization).get_token_headers() # Extract token headers
+    HeaderValidator(headers=token_headers).validate_headers() # Validate token headers
+    token_payload = await SignatureValidator(token=Authorization, x5u=token_headers["x5u"]).validate_signature() # Validate token signature
+    ExpirationValidator(
+        token_payload=token_payload).validate_expiration() # Validate custom token expiration (max time to live)
+    return JWTValidity(valid=True) # Return a valid response
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
